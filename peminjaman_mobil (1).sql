199a9b2d6814449d644a991cb12f18d20816acae
iqbalakbark <151427933+iqbalakbark@users.noreply.github.com>
2024-12-19 20:16:27 +0700

    Add files via upload

---
+++ peminjaman_mobil (1).sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Des 2024 pada 14.16
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `peminjaman_mobil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `id` int(11) NOT NULL,
+  `nama_kendaraan` varchar(255) NOT NULL,
+  `no_polisi` varchar(50) NOT NULL,
+  `jenis_kendaraan` varchar(100) NOT NULL,
+  `status` enum('Tersedia','Dipinjam') DEFAULT 'Tersedia'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `mobil`
+--
+
+INSERT INTO `mobil` (`id`, `nama_kendaraan`, `no_polisi`, `jenis_kendaraan`, `status`) VALUES
+(1, 'Toyota Avanza', 'B1234XYZ', 'Mobil Penumpang', 'Tersedia');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','user') NOT NULL DEFAULT 'user',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `created_at`) VALUES
+(1, 'iqbal akbar kurniawan', 'iqbale.ak26@gmail.com', '$2y$10$m4UI8/Xl61NLv.MbzeD3oeswYbPMmazgQAqMCttV0.fFc.hD8u6V2', 'admin', '2024-12-18 00:29:52'),
+(2, 'Admin User', 'admin@example.com', 'hashedpassword', 'admin', '2024-12-18 06:33:43'),
+(5, 'ebin', 'ebin@gmail.com', '$2y$10$cO4gO/rfowyK2kR6cRe3QeyMlNw8VINSf0beOWqaFbHzZBoB.JaSi', 'user', '2024-12-18 01:32:24');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `no_polisi` (`no_polisi`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mobil`
+--
+ALTER TABLE `mobil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
